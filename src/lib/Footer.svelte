fb8c9bec92f36446707154371a4a92e72ddb5e6f
kubow <kube.kubow@gmail.com>
2024-12-31 02:59:51 +0100

    navbar and footer added

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,21 @@
+<footer class="bg-light py-5">
+    <div class="container px-4 px-lg-5">
+        <small class="text-center text-muted">Copyright &copy; 2025 - Kefer Astrology</small>
+    </div>
+</footer>
+
+<style>
+    footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,81 @@
+<script>
+    // Functionality for language selection
+    import { t, currentLanguage, toggleLanguage } from '$lib/stores/languageStore';
+    
+</script>
+  
+<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3">
+    <div class="container px-4 px-lg-5">
+      <a class="navbar-brand" href="#page-top">Kefer Astrology</a>
+      <div id="navbarResponsive" class="collapse navbar-collapse">
+        <ul class="navbar-nav ms-auto my-2 my-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#about">{t('about')}</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#function">{t('functions')}</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#download">{t('download')}</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">{t('contact')}</a>
+          </li>
+          <li>
+            <button type="button" class="lang-switch" on:click={toggleLanguage}>
+                {$currentLanguage === 'cs' ? 'en' : 'cs'}
+              </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+</nav>
+  
+<style>
+    /* Navbar */
+    nav {
+    background: white;
+    border-bottom: 1px solid #ddd;
+    position: fixed;
+    width: 100%;
+    z-index: 10;
+    padding: 1rem 2rem;
+  }
+
+  nav ul {
+    list-style: none;
+    display: flex;
+    justify-content: flex-end;
+    gap: 1.5rem;
+    margin: 0;
+    padding: 0;
+  }
+
+  nav a {
+    text-decoration: none;
+    color: #000;
+    font-weight: bold;
+  }
+
+  nav a:hover {
+    color: #007bff;
+  }
+
+
+.lang-switch {
+    background: none;
+    border: none;
+    color: #000;
+    cursor: pointer;
+    padding: 0.5rem 1rem;
+    font-weight: bold;
+    margin-left: 0.5rem;
+    color: #007bff;
+    text-decoration: underline;
+  }
+
+  .lang-switch:hover {
+    color: #0056b3;
+  }
+
+</style>
