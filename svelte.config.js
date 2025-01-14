e0f47214442c0eb369fe3c45acaa05ab6d5993c1
kubow <kube.kubow@gmail.com>
2025-01-14 20:13:14 +0100

    static site rendering adapter introduced

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+// TO-DO check for static adapter !
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +13,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({pages: "docs", assets: "docs", fallback: "404.html"}),
+		appDir: "scripts",
+		paths: {
+			base: process.argv.includes('dev') ? '' : "/keferastrology.com",
+		}
 	}
 };
 
