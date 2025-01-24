4ba86ea2797140a84e61bb3b950906758816f80c
kubow <kube.kubow@gmail.com>
2025-01-24 02:33:15 +0100

    regenerated latest preview

---
+++ docs/scripts/immutable/chunks/paths.UwQlxKe0.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_1t2pu3i)==null?void 0:s.base)??"/keferastrology.com";var t;const a=((t=globalThis.__sveltekit_1t2pu3i)==null?void 0:t.assets)??e;export{a,e as b};
--- docs/scripts/immutable/entry/start.CsKeA16C.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.D6vi1EDg.js";export{t as start};
